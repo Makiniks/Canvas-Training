e555028b2143078662c0b2e0c0b2ed8f89294293
Makiniks <makiniks.online@gmail.com>
2020-07-09 02:21:39 +0000

    Online 1

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ app.use(express.static('public'))
 
 const game = createGame()
 
-game.addPlayer({playerId: 'player1', playerX: 0, playerY: 0})
-game.addFruit({fruitId: 'fruit1', fruitX: 5, fruitY:5})
-
-game.movePlayer({playerId: 'player1', keyPressed: 'ArrowRight'})
-
 sockets.on('connection', (socket) => {
 
     const playerId = socket.id
