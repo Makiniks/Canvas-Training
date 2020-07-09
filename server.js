01426097945b7ad61fb10dd3a330160b025737d9
Makiniks <makiniks.online@gmail.com>
2020-07-09 01:27:04 +0000

    Online 1

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from 'express'
 import http from 'http'
+import createGame from './public/game.js'
+import socketio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
+const sockets = socketio(server)
 
 app.use(express.static('public'))
+
+const game = createGame()
+
+game.addPlayer({playerId: 'player1', playerX: 0, playerY: 0})
+game.addFruit({fruitId: 'fruit1', fruitX: 5, fruitY:5})
+
+game.movePlayer({playerId: 'player1', keyPressed: 'ArrowRight'})
+
+console.log(game.state)
+
+sockets.on('connection', (socket) => {
+
+    const playerId = socket.id
+
+    console.log(`Player connected on Server with id: ${playerId}`)
+
+})
 
 server.listen(3000, () => {
 
