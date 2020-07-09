95b3d02a41a6fb5cfe6991edb940f323858a6d9f
Makiniks <makiniks.online@gmail.com>
2020-07-09 02:12:49 +0000

    Online 1

--- public/game.js
+++ public/game.js
@@ -22,8 +22,8 @@ export default function createGame() {
     function addPlayer(command) {
 
         const playerId = command.playerId
-        const playerX = 'playerX' in command ? comand.playerX : Math.floor(Math.random = state.screen.width)
-        const playerY = 'playerY' in command ? comand.playerY : Math.floor(Math.random = state.screen.height)
+        const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random = state.screen.width)
+        const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random = state.screen.height)
 
         state.players[playerId] = {
 
